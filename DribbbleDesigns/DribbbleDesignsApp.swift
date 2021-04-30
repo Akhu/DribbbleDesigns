8d7899dd2e425e854e84fe24531e9edf3fe4dea6
Anthony <me@anthony-dacruz.com>
2021-04-30 11:35:45 +0200

    Initial Commit

---
+++ DribbbleDesigns/DribbbleDesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbbleDesignsApp.swift
+//  DribbbleDesigns
+//
+//  Created by Anthony Da cruz on 30/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DribbbleDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
